21ee26cbadf93c3fe5a4082fd316919b975956ef
Ronny <ronnydougla2@gmail.com>
2024-03-31 00:38:33 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  const listArray = [];
+  $("#register").click("submit", function (event) {
+    event.preventDefault();
+
+    const toList = $("#task").val();
+
+    if (toList === "") {
+      alert("Campo vazio");
+      return;
+    }
+
+    if (listArray.includes(toList)) {
+      alert("Essa lista já foi adicionada");
+      $("#task").val("");
+    } else {
+      listArray.push(toList);
+      const createElement = $(`<li>${toList}</li>`);
+      createElement.appendTo("#list");
+      $("#task").val("");
+    }
+  });
+
+  $("#list").on("click", "li", function () {
+    $(this).css("text-decoration", "line-through");
+  });
+});
